087fe714ad72d5a0f12ace2944a0668f447a56b8
HuyPham <pham.xuan.huy@mqsolutions.vn>
2018-04-09 11:58:03 +0700

    Display output in React with a Component's render method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { SSL_OP_PKCS1_CHECK_1 } from 'constants';
 
-// class App extends React.Component {
-//   render() {
-//     // return <h1>Hello World</h1>
-//     return React.createElement('h1', null, 'Hello World!')
-//   }
-// }
+class App extends React.Component {
+  render() {
+    // return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
-const App = () => <h1>Hello Stateless</h1>
+// const App = () => <h1>Hello Stateless</h1>
 
 export default App
