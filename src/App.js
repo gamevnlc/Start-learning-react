10adcd8b66f9870a536d516595bd765c6bcd988a
HuyPham <pham.xuan.huy@mqsolutions.vn>
2018-04-09 15:01:17 +0700

    set properties on a component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class App extends React.Component {
   render() {
     // return <h1>Hello World</h1>
+    let txt = this.props.txt
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>{txt}</h1>
         <b>Bold</b>
       </div>
     )
   }
+}
+
+// Define prop
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.string.isRequired
+}
+
+// Default prop
+
+App.defaultProps = {
+  txt: 'HIhi'
 }
 
 // const App = () => <h1>Hello Stateless</h1>
