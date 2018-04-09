b9ac924ace77a3f4632ae2fe51936b7fd41c44a1
HuyPham <pham.xuan.huy@mqsolutions.vn>
2018-04-09 11:44:29 +0700

    write hello world

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { SSL_OP_PKCS1_CHECK_1 } from 'constants';
 
-const App = () => <h1>Hello World</h1>
+// class App extends React.Component {
+//   render() {
+//     // return <h1>Hello World</h1>
+//     return React.createElement('h1', null, 'Hello World!')
+//   }
+// }
+
+const App = () => <h1>Hello Stateless</h1>
 
 export default App
