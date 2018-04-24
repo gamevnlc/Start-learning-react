7a10960bba1fadd58391a6eebf74d96a766ca858
HuyPham <gracelord0705@gmail.com>
2018-04-24 11:43:38 +0700

    Build a JSX Live Compiler as A React Component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App cat="Haleluy"/>,
+ReactDOM.render(<App/>,
     document.getElementById('root')
 );
 // registerServiceWorker();
